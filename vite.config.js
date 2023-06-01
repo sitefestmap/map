bd089d5edd1bd85e85c5404d2ca8be81006ba320
mattweston <68040892+mattweston@users.noreply.github.com>
2023-06-01 18:58:38 +0100

    redirects

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    //polyfillDynamicImport: true,
     rollupOptions: {
       // root: './src/pages'
       input: {
